425ff573e9b3b7da8748ec63e63245a8b005661e
Samin Yasar <yasarsamin57@gmail.com>
2025-08-21 04:55:31 +0600

    dev: default admin user credentials added in dotenv

--- src/config/dotenv.config.ts
+++ src/config/dotenv.config.ts
@@ -6,3 +6,11 @@ export const dbConnectionString: string =
   process.env.DB_CONNECTION_STRING || "";
 export const jwtSecret: string = process.env.JWT_SECRET || "";
 export const jwtExpiresIn: number = parseInt(process.env.JWT_EXPIRES_IN || "");
+export const defaultAdminName: string = process.env.DEFAULT_ADMIN_NAME || "";
+export const defaultAdminUsername: string =
+  process.env.DEFAULT_ADMIN_USERNAME || "";
+export const defaultAdminEmail: string = process.env.DEFAULT_ADMIN_EMAIL || "";
+export const defaultAdminPassword: string =
+  process.env.DEFAULT_ADMIN_PASSWORD || "";
+export const defaultAdminContactNumber: string =
+  process.env.DEFAULT_ADMIN_CONTACT_NUMBER || "";
