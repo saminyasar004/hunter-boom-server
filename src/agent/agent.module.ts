82532141b9bbdd81e76efd82ff608dd01c52a466
Samin Yasar <yasarsamin57@gmail.com>
2025-08-21 14:50:13 +0600

    dev: agent module added

--- src/agent/agent.module.ts
+++ src/agent/agent.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AgentService } from './agent.service';
-import { AgentController } from './agent.controller';
+import { Module } from "@nestjs/common";
+import { AgentService } from "./agent.service";
+import { AgentController } from "./agent.controller";
+import { SequelizeModule } from "@nestjs/sequelize";
+import Agent from "@/model/agent.model";
 
 @Module({
+  imports: [SequelizeModule.forFeature([Agent])],
   controllers: [AgentController],
   providers: [AgentService],
 })
